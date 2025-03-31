4a55d8de6dd2486a4288e7dd91e22ff6640006f6
SeangHai12 <hengseanghai032@gmail.com>
2025-03-31 17:56:32 +0700

    commit

---
+++ Section_14/bmi.js
@@ -0,0 +1,6 @@
+function bmicalcualtor(weight, height) {
+  var bmi = Math.floor(weight / (height * height));
+  console.log("Bmi : " + bmi);
+}
+
+bmicalcualtor(55, 1.7);
--- Section_14/khop.js
+++
@@ -1,2 +0,0 @@
-window.alert("Ello");
-1;
